41ed31947d733b269baf1e2c88abe88a1764b2ff
sathwiksyr <sathwiksyr@gmail.com>
2026-03-19 20:19:47 +0530

    Fix data path in loader

--- ChurnIQ/loader.py
+++ ChurnIQ/loader.py
@@ -9,7 +9,7 @@ import streamlit as st
  
 @st.cache_resource
 def load_model():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     model    = pickle.load(open(os.path.join(BASE_DIR, "model", "churn_model.pkl"), "rb"))
     encoders = pickle.load(open(os.path.join(BASE_DIR, "model", "encoders.pkl"),    "rb"))
     return model, encoders
@@ -17,5 +17,6 @@ def load_model():
  
 @st.cache_data
 def load_data():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return pd.read_csv(os.path.join(BASE_DIR, "data", "Churn.csv"))
+ 
